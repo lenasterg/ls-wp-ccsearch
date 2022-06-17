f61325398ae77720db4735d294a89cbd15bff827
Vagelis Papaioannou <hello@vagelis.dev>
2022-06-17 15:10:41 +0300

    updated block texts, updated mo and po files

--- blocks/build/index.asset.php
+++ blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '9a8b1912442d6a43d856b5857f06c45e');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '7fce3a42ce953f83963d34ef9b8c36b0');
